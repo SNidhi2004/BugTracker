567b340dad3f63c169a1da81d9e21e429b93352a
SNidhi2004 <nidhi4sp@gmail.com>
2025-07-29 13:46:55 +0530

    Initial deploy setup

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("thisisforlogginin") or "thisisforlogginin"
-    MONGO_URI = os.environ.get("mongodb://localhost:27017/bugtracker") or "mongodb://localhost:27017/bugtracker"
+    MONGO_URI = os.environ.get("mongodb+srv://bugtrackeruser:bugtrackeruser1234@myatlasclusteredu.fuxls.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU") or "mongodb+srv://bugtrackeruser:bugtrackeruser1234@myatlasclusteredu.fuxls.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU"
 
